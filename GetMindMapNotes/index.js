9c2ea7ed63197b536e68f12e8cd111bb54df47aa
Mukesh <mukeshsharma20120@gmail.com>
2025-02-25 19:47:53 +0530

    make it more dynamic to upload the mindmap

--- GetMindMapNotes/index.js
+++ GetMindMapNotes/index.js
@@ -75,6 +75,87 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+
+const createJSXFile = (folderPath) => {
+  const filename = folderPath.split("\\").pop() || "constantData";
+  const FileNameUpperCase = filename.charAt(0).toUpperCase() + filename.slice(1);
+// Create the new .jsx file with the provided content
+const jsxContent = `
+import { useState } from "react";
+import logo from "../../assets/${filename.toLowerCase()}.png";
+import { notesData, coordsData } from "../../constant/${filename.toLowerCase()}";
+import "./common.css";
+
+export default function ${FileNameUpperCase}() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setModalContent("");
+  };
+
+  const handleAreaClick = (noteKey) => (event) => {
+    event.preventDefault();
+    setModalContent(notesData[noteKey]);
+    setModalVisible(true);
+  };
+
+  return (
+    <div className="container">
+      {modalVisible && (
+        <div onClick={closeModal} className="modal-container">
+          <div className="modal-content">
+            <div className="modal-close-container">
+              <p onClick={closeModal} className="modal-close">
+                &times;
+              </p>
+            </div>
+            <div className="text-white text-xl" dangerouslySetInnerHTML={{ __html: modalContent.body }} />
+          </div>
+        </div>
+      )}
+
+      <img src={logo} alt="image" border="none" useMap="#map" />
+      <map name="map">
+        {coordsData.map((item) => (
+          <>
+            <area
+              shape="rect"
+              coords={item.coords}
+              href="#"
+              onClick={handleAreaClick(item.noteKey)}
+            />
+          </>
+        ))}
+      </map>
+    </div>
+  );
+}
+`;
+
+  const jsxFileName = `D:\\Real Projects\\My Projects\\MindMapping\\Frontend\\src\\components\\Maps\\${FileNameUpperCase}.jsx`;
+  fs.writeFileSync(jsxFileName, jsxContent, "utf-8");
+
+  console.log("JSX file created at ", jsxFileName);
+}
+
+
+// Function to copy image to assets folder
+function copyImageToAssets(folderPath) {
+  const folderName = folderPath.split("\\").pop().toLowerCase();
+  const sourceImagePath = path.join(folderPath, `${folderName}.png`);
+  const destinationImagePath = `D:\\Real Projects\\My Projects\\MindMapping\\Frontend\\src\\assets\\${folderName}.png`;
+
+  try {
+    fs.copyFileSync(sourceImagePath, destinationImagePath);
+    console.log(`Image copied to ${destinationImagePath}`);
+  } catch (error) {
+    console.error(`Error copying image: ${error.message}`);
+  }
+}
+
+
 rl.question("Enter the path of your project folder: ", (folderPath) => {
   // const folderPath = "D:/system design2/notes";
   const notes = extractNotes(`${folderPath}/notes`);
@@ -87,9 +168,12 @@ rl.question("Enter the path of your project folder: ", (folderPath) => {
   )}\n\n export const coordsData = ${JSON.stringify(coords, null, 2)}`;
 
   // Write the file to a JavaScript file (notesData.js)
-  fs.writeFileSync("notesData.js", fileContent, "utf-8");
-
-  console.log("Data saved to notesData.js");
+  const filename = folderPath.split("\\").pop().toLowerCase() || "constantData";
+  const constantFileName = `D:\\Real Projects\\My Projects\\MindMapping\\Frontend\\src\\constant\\${filename}.js`;
+  fs.writeFileSync(constantFileName, fileContent, "utf-8");
+  createJSXFile(folderPath);
+  copyImageToAssets(folderPath);
+  console.log("Data saved to ", filename);
 
   rl.close();
 });
